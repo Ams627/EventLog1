c0e973dc043f1006ee913e41574a1fdd4f37ddfc
Adrian Sims <adriansims62781@gmail.com>
2018-02-04 19:15:23 +0000

    Add project files.

---
+++ EventLog1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventLog1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+
+                string eventID1 = "6005";
+                string eventID2 = "6006";
+                string LogSource = "System";
+                string sQuery = $"*[System/EventID=\"{eventID1}\" or System/EventID=\"{eventID2}\"]";
+
+                Console.WriteLine($"Query {sQuery}");
+
+                var elQuery = new EventLogQuery(LogSource, PathType.LogName, sQuery);
+                var elReader = new System.Diagnostics.Eventing.Reader.EventLogReader(elQuery);
+                List<EventRecord> eventList = new List<EventRecord>();
+                for (EventRecord eventInstance = elReader.ReadEvent();
+                    null != eventInstance; eventInstance = elReader.ReadEvent())
+                {
+                    eventList.Add(eventInstance);
+                    string eventName = "No event";
+                    if (eventInstance.Id == 6005)
+                    {
+                        eventName = "Computer startup";
+                    }
+                    else if (eventInstance.Id == 6006)
+                    {
+                        eventName = "Computer shutdown";
+                    }
+                    Console.WriteLine($"{eventName}: {eventInstance.TimeCreated}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add EventLog1/Program.cs
+git add EventLog1/App.config
+git add EventLog1/Properties/AssemblyInfo.cs
+git add EventLog1/EventLog1.csproj
+git add EventLog1.sln
+git add .gitignore
